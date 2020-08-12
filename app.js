c7cf8c25399db3e4c328b5331c6f0bdd595a448f
FreakyHarsh <harsh.boricha2015@gmail.com>
2020-08-12 17:48:00 +0530

    deploy test2

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
