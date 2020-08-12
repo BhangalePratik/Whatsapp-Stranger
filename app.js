1369b55d2b89747dc54f7e1171f06730226af05d
FreakyHarsh <harsh.boricha2015@gmail.com>
2020-08-12 16:59:30 +0530

    barebone mmodel

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.post('/generate-link', (req, res, next) => {
+    let wsNumber = req.body.number;
+    let msg = req.body.msg ? req.body.msg : '';
+    res.render('./redirect', {
+        wsNumber,
+        msg
+    })
+});
+
+app.use('/', (req, res, next) => {
+    res.render('./index')
+});
+
+app.listen(3000);
