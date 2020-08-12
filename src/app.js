c5fa0d23a85a365ac6a2162633ef3e7c52e89580
FreakyHarsh <harsh.boricha2015@gmail.com>
2020-08-12 17:34:13 +0530

    added router

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
 
 const app = express();
-
+const router = express.Router();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/.netlify/functions/', router);
 
-
-app.post('/generate-link', (req, res, next) => {
+router.post('/generate-link', (req, res, next) => {
     let wsNumber = req.body.number;
     let msg = req.body.msg ? req.body.msg : '';
     res.render('./redirect', {
@@ -17,8 +17,9 @@ app.post('/generate-link', (req, res, next) => {
     })
 });
 
-app.use('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.render('./index');
 });
+
 module.exports.handler = serverless(app);
 app.listen(3000);
